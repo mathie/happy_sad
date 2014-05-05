863994301332130c1d1787d8c7b091f7dfd303f0
Graeme Mathieson <mathie@woss.name>
2014-05-05 15:32:13 +0100

    Use Puma as an app server in development.

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,8 @@
+# Force stdio to be unbuffered, even when the controlling tty isn't a terminal.
+# This means that logs show up immediately in foreman's output.
+$stdout.sync = true
+$stderr.sync = true
+
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
