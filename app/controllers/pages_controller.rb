f0498c765d7183c21ad2d10ee60fb21c5b204ea1
Graeme Mathieson <mathie@woss.name>
2014-05-06 10:43:38 +0100

    An initial stab at generating documentation with RDoc.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
+##
+# A controller for rendering (mostly) static pages, like the home page. There
+# may still be dynamic content on the pages, but the content should be generic
+# and entirely cacheable. The +index+ action is the home page. Most of the rest
+# of the pages are named consistently between the route and the action.
 class PagesController < ApplicationController
+
+  ##
+  # The Happy/Sad home page, with some marketing copy encouraging people to
+  # sign up.
   def index
   end
 end
